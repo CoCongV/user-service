9fa4833b2001aa486f257096baccd65ea1aac19c
CoCong <cong.lv.yx@gmail.com>
2021-07-07 13:21:04 +0800

    fix unit test

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ extern crate clap;
 extern crate dotenv;
 extern crate lazy_static;
 
+use std::io;
+
 use actix_web::{middleware, App, HttpServer};
 use clap::SubCommand;
 use diesel::PgConnection;
@@ -37,28 +39,32 @@ async fn runserver() -> std::io::Result<()> {
 
 fn init(conn: &PgConnection) -> std::io::Result<()> {
     let _conf = CONF.clone();
-    models::user::insert_new_user(_conf.admin_user, _conf.admin_email, _conf.admin_password, _conf.default_avatar, conn).unwrap();
+    models::user::insert_new_user(
+        _conf.admin_user,
+        _conf.admin_email,
+        _conf.admin_password,
+        _conf.default_avatar,
+        conn,
+    )
+    .unwrap();
     Ok(())
 }
 
 fn main() {
-    let pool = db::create_db_pool(&CONF.db_url);
     let matches = clap::App::new("User Service")
         .subcommand(SubCommand::with_name("runserver").about("run server"))
         .subcommand(SubCommand::with_name("init").about("init database and create admin uesr"))
         .get_matches();
-    
-        if let Some(_) = matches.subcommand_matches("runserver") {
-            if let Ok(_) = runserver() {
-            }
+    if let Some(_) = matches.subcommand_matches("runserver") {
+        if let Ok(_) = runserver() {}
+    }
+    if let Some(_) = matches.subcommand_matches("init") {
+        let pool = db::create_db_pool(&CONF.db_url);
+        let conn = pool.get().expect("get diesel connection fail!");
+        if let Ok(_) = init(&conn) {
+            println!("init success!")
         }
-
-        if let Some(_) = matches.subcommand_matches("init") {
-            let conn = pool.get().expect("get diesel connection fail!");
-            if let Ok(_) = init(&conn) {
-                println!("init success!")
-            }
-        }
+    }
 }
 
 #[cfg(test)]
@@ -70,9 +76,14 @@ mod tests {
     use crate::api::token::generate_auth_token;
     use crate::interface::Info;
 
+    lazy_static! {
+        static ref POOL: db::Pool = db::create_db_pool(&CONF.db_url);
+    }
+    
     #[actix_rt::test]
     async fn test_get_token_ok() {
-        let mut app = test::init_service(App::new().service(generate_auth_token)).await;
+        let mut app =
+            test::init_service(App::new().data(POOL.clone()).service(generate_auth_token)).await;
         let req = test::TestRequest::get()
             .uri("/api/v1/token")
             .set_json(&Info {
